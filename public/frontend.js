26d9cc58453c2946d54969436b0a7c14cafbb769
rajohnsn <nathan.rajohnson@grenoble-inp.org>
2019-11-07 14:50:47 +0100

    initial commit -assets from codename-

---
+++ public/frontend.js
@@ -0,0 +1,172 @@
+$(function () {
+	"use strict";
+	
+	// for better performance - to avoid searching in DOM
+	var grille = $('#grille');
+	var form = $('#form');
+	var monEquipe = "none";
+	var espion = false;
+	var debutPartie = false;
+	
+	for (let i = 0; i < 5; i++) {
+		for (let j = 0; j < 5; j++) {
+			grille.append('<span class="case" id="case_' + i + '_' + j + '"></span>');
+		}
+	}
+	
+	// if user is running mozilla then use it's built-in WebSocket
+	window.WebSocket = window.WebSocket || window.MozWebSocket;
+	
+	// if browser doesn't support WebSocket, just show some notification and exit
+	if (!window.WebSocket) {
+		content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+		+ 'support WebSockets.'} ));
+		input.hide();
+		$('span').hide();
+		return;
+	}
+	
+	// open connection
+	var connection = new WebSocket('wss://' + window.location.hostname + "");
+	
+	connection.onopen = function () {
+		var start = {};
+		new Promise((resolve) => {
+			start.type = "start";
+			start.room = prompt('Entrez une salle');
+			start.espion = confirm('Etes vous espion ?');
+			espion = start.espion;
+			resolve();
+		}).then(() => {
+			connection.send(JSON.stringify(start));
+		});
+	};
+	
+	connection.onerror = function (error) {
+		// just in there were some problems with conenction...
+		$('body').html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+		+ 'connection or the server is down.' } ));
+	};
+	
+	// most important part - incoming messages
+	connection.onmessage = function (message) {
+		try {
+			var json = JSON.parse(message.data);
+		} catch (e) {
+			console.log('This doesn\'t look like a valid JSON: ', message.data);
+			return;
+		}
+		
+		// On met la couleur de l'equipe en fond
+		if (json.type === 'equipe') {
+			monEquipe = json.equipe;
+			if (monEquipe === "bleu") {
+				$('body').addClass('blue');
+			} else if (monEquipe === "rouge") {
+				$('body').addClass('red');
+			}
+		}
+		// On définit le tour
+		else if (json.type === 'tour') {
+			if (json.tour === monEquipe || monEquipe === 'display') {
+				monTour = true;
+			} else {
+				monTour = false;
+			}
+		}
+		// On met le mot dans la bonne case (case labelisée case_i_j)
+		else if (json.type === 'mot') {
+			$("#case_" + json.casei + "_" + json.casej).text(json.mot);
+		}
+		// Case cochée
+		else if (json.type === 'click') {
+			$("#case_" + json.casei + "_" + json.casej).addClass('revele');
+			$("#case_" + json.casei + "_" + json.casej).addClass('raye');
+		}
+		// Début de partie
+		else if (json.type === 'debutPartie') {
+			form.hide();
+			debutPartie = true;
+
+			$('.case').removeAttr('class').addClass('case');
+			if (espion) {
+				$('.case').addClass('revele');
+			}
+
+			json.room.rouges.forEach(mot => {
+				let casei = parseInt(mot / 5);
+				let casej = mot % 5;
+				$("#case_" + casei + "_" + casej).addClass("rouge");
+			});
+			json.room.bleus.forEach(mot => {
+				let casei = parseInt(mot / 5);
+				let casej = mot % 5;
+				$("#case_" + casei + "_" + casej).addClass("bleu");
+			});
+			json.room.dead.forEach(mot => {
+				let casei = parseInt(mot / 5);
+				let casej = mot % 5;
+				$("#case_" + casei + "_" + casej).addClass("dead");
+			});
+		}
+		// Fin de partie on vide les cases
+		else if (json.type === 'finPartie') {
+			form.show();
+			debutPartie = false;
+		}
+		
+		// Fin de partie on vide les cases
+		else if (json.type === 'notEspion') {
+			espion = false;
+		}
+		else {
+			console.log('Hmm..., I\'ve never seen JSON like this: ', json);
+		}
+	};
+	
+	form.submit(function (e) {
+		e.preventDefault();
+		let mot = {};
+		mot.type = "mot";
+		mot.mot = $('#mot').val();
+		$('#mot').val('');
+		if (mot.mot.match(/^[a-zA-Z\-]+$/g)) {
+			mot.mot = mot.mot.toLowerCase();
+			connection.send(JSON.stringify(mot));
+		} else {
+			alert('Sans caractere chelou stp');
+		}
+	});
+
+	$('.allrandom').on('click', function() {
+		$.get("public/mots.txt", function(wholeTextFile) {
+			let lines = wholeTextFile.split(/\n/);
+			let mot = {};
+			mot.type = "mot";
+			let randomIndex;
+
+			for (let i = 0; i < 30; i++) {
+				randomIndex = Math.floor(Math.random() * lines.length);
+				mot.mot = lines[randomIndex];
+				connection.send(JSON.stringify(mot));
+			}
+		  })
+	});
+
+	$('.random').on('click', function() {
+		$.get("public/mots.txt", function(wholeTextFile) {
+			let lines = wholeTextFile.split(/\n/),
+			  randomIndex = Math.floor(Math.random() * lines.length);
+			$('#mot').val(lines[randomIndex]);
+		  })
+	});
+	
+	$('.case').on('click', function() {
+		if(espion === false && debutPartie === true) {
+			let choix = {};
+			choix.type = "click";
+			choix.click = parseInt($(this).attr('id').split("_")[1]) * 5 + parseInt($(this).attr('id').split("_")[2]);
+			connection.send(JSON.stringify(choix));
+		}
+	});
+});
---
+++ server.js
@@ -0,0 +1,189 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const WebSocket = require('ws');
+const http = require('http');
+const request = require('request');
+
+// Optional. You will see this name in eg. 'ps' or 'top' command
+process.title = 'node-server';
+
+
+//initialize a simple http server
+const server = http.createServer(app);
+var wsServer = new WebSocket.Server({
+	server: server
+});
+
+router.get('/',function(req,res){
+	res.sendFile(path.join(__dirname+'/frontend.html'));
+});
+
+app.use('/public', express.static('public'));
+
+
+//add the router
+app.use('/', router);
+
+//start our server
+server.listen(process.env.PORT || 1337, () => {
+	console.log(`Server started on port ${server.address().port} :)`);
+});
+
+/**
+* Global variables
+*/
+// list of currently connected clients (users)
+var clients = [ ];
+
+var rooms = {};
+
+
+// This callback function is called every time someone
+// tries to connect to the WebSocket server
+wsServer.on('connection', function(connection) {
+	
+	console.log((new Date()) + ' Connection');
+	
+	// we need to know client index to remove them on 'close' event
+	var index = clients.push(connection) - 1;
+	var espion = false;
+	var room = "";
+	var equipe = 0;
+
+	setInterval(() => {connection.ping()}, 10000);
+	
+	
+	// user sent some message
+	connection.on('message', function(message) {
+		try {
+			var json = JSON.parse(message);
+		} catch (e) {
+			console.log('This doesn\'t look like a valid JSON: ', message);
+			return;
+		}
+		
+		
+		if (json.type === 'start') {
+			room = json.room;
+			espion = json.espion;
+			
+			if (rooms[room] === undefined) {
+				rooms[room] = {"clients": [], "espions": [], "mots": [], "rouges": [], "dead" : [], "bleus": [], "blancs" : [], "done": []};
+			}
+
+			rooms[room].clients.push(connection);
+			if(espion && rooms[room].espions.indexOf("rouge") < 0) {
+				equipe = rooms[room].espions.push("rouge");
+				connection.send(JSON.stringify({"type" : "equipe", "equipe": "rouge"}));
+			}
+			else if(espion && rooms[room].espions.indexOf("bleu") < 0) {
+				equipe = rooms[room].espions.push("bleu");
+				connection.send(JSON.stringify({"type" : "equipe", "equipe": "bleu"}));
+			} 
+			else {
+				connection.send(JSON.stringify({"type" : "notEspion"}));
+				espion = false;
+			}
+			
+			for (const indexMot in rooms[room].mots) {
+				if (rooms[room].mots.hasOwnProperty(indexMot)) {
+					let res = {"type" : "mot", "mot": rooms[room].mots[indexMot]}; 
+					
+					res.casei = parseInt(indexMot / 5);
+					res.casej = (indexMot) % 5;
+					
+					connection.send(JSON.stringify(res));
+				}
+			}
+
+			if (rooms[room].mots.hasOwnProperty(24)) {
+				connection.send(JSON.stringify({"type" : "debutPartie", "room": rooms[room]}));
+				for (var i=0; i < rooms[room].done.length; i++) {						
+					rooms[room].clients[i].send(JSON.stringify({"type" : "click", "casei": parseInt(rooms[room].done[i] / 5), "casej" : rooms[room].done[i] % 5}));
+				}
+			}
+		}
+		else if (rooms[room] !== undefined && rooms[room].clients !== undefined && rooms[room].espions !== undefined && rooms[room].mots !== undefined && rooms[room].rouges !== undefined && rooms[room].dead !== undefined && rooms[room].blancs !== undefined && rooms[room].bleus !== undefined && rooms[room].done !== undefined)  {
+			if (json.type === 'mot') {
+
+				if  (rooms[room].mots.length < 25) {
+					// On verifie que le mot n'est pas déjà présent
+					if (rooms[room].mots.indexOf(json.mot) < 0) {
+						let iCase = rooms[room].mots.push(json.mot) - 1;
+						
+						json.casei = parseInt(iCase / 5);
+						json.casej = (iCase) % 5;
+						
+						for (var i=0; i < clients.length; i++) {
+							clients[i].send(JSON.stringify(json));
+						}
+					}
+
+					if(rooms[room].mots.length === 25) {
+						let aRepartir = [];
+						for (let i = 0; i < 25; i++) {
+							aRepartir.push(i);
+						}
+						shuffleArray(aRepartir);
+						rooms[room].rouges = aRepartir.slice(0, 9);
+						rooms[room].bleus = aRepartir.slice(9, 17);
+						rooms[room].dead = aRepartir.slice(17, 18);
+						rooms[room].blancs = aRepartir.slice(18, 25);
+						rooms[room].done = [];
+
+						for (var i=0; i < rooms[room].clients.length; i++) {						
+							rooms[room].clients[i].send(JSON.stringify({"type" : "debutPartie", "room": rooms[room]}));
+						}
+					}
+				}
+				else {					
+					connection.send(JSON.stringify({"type" : "debutPartie", "room": rooms[room]}));
+				}
+			}
+			else if (json.type === 'click') {
+				rooms[room].done.push(parseInt(json.click));
+
+				for (var i=0; i < rooms[room].clients.length; i++) {						
+					rooms[room].clients[i].send(JSON.stringify({"type" : "click", "casei": parseInt(json.click / 5), "casej" : json.click % 5}));
+				}
+
+				if (rooms[room].bleus.every((val) => rooms[room].done.includes(val))  || rooms[room].rouges.every((val) => rooms[room].done.includes(val)) || rooms[room].done.includes(rooms[room].dead[0])) {
+					for (var i=0; i < rooms[room].clients.length; i++) {						
+						rooms[room].clients[i].send(JSON.stringify({"type" : "finPartie"}));
+						rooms[room].mots = [];
+						rooms[room].done = [];
+					}
+				}
+			}
+		}
+	});
+	
+	// user disconnected
+	connection.on('close', function(connection) {
+		console.log((new Date()) + " Peer disconnected.");
+		// remove user from the list of connected clients
+		if (espion) {
+			rooms[room].espions[equipe - 1] = "";
+		}
+		rooms[room].clients.splice(rooms[room].clients.indexOf(connection), 1);
+		clients.splice(clients.indexOf(connection), 1);
+
+		if (rooms[room].clients.length === 0) {
+			rooms[room].mots = [];
+			rooms[room].done = [];
+			rooms[room].espions = [];
+		}
+	});
+	
+});
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}

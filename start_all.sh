d5cdd770acfc1b2b3e22bd3d38ccc5a9b1f61f9d
raffraye <elouan.raffray@etu.esisar.grenoble-inp.fr>
2020-03-06 13:01:54 +0100

    fix start all

--- start_all.sh
+++ start_all.sh
@@ -6,6 +6,7 @@ cd word2vec_server
 exec 3< <(node server.js)
 sed '/\(null\)$/q' <&3 ; cat <&3 &
 cd ../web_server
-exec 3< <(node server.js)
-sed '/Word Client connected$/q' <&3 ; cat <&3 &
+exec 4< <(node server.js)
+sed '/Server started on port 8080$/q' <&4 ; cat <&4 &
+cd ..
 ./display/application.linux-armv6hf/display $1 $2
